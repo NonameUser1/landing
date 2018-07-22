9b72955095f9a832d7fa90cd4e9d526c809a97d6
Tanya Bilanyuk <34652839+spookyTanya@users.noreply.github.com>
2018-07-22 21:35:11 +0300

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-
 import Shapka from './components/first/shapka.js';
+import Second from './components/second/second.js';
+import Third from './components/third/third.js';
+import Fourth from './components/fourth/fourth.js';
+import Tenth from './components/tenth/tenth.js';
+
 
 class App extends Component{
 	render(){
 		return(
 			<div>
-			<Shapka/>
+				<Shapka/>
+				<Second />
+				<Third /> 
+				<Fourth />
+				<Tenth />
 			</div>
 		 );
 		
