95420eaf984797e1afe6fe42a8557480f070d49e
Tanya Bilanyuk <34652839+spookyTanya@users.noreply.github.com>
2018-08-02 23:18:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Shapka from './components/first/shapka.js';
 import Second from './components/second/second.js';
 import Third from './components/third/third.js';
 import Fourth from './components/fourth/fourth.js';
+import Sixth from './components/sixth/sixth.js';
 import Tenth from './components/tenth/tenth.js';
 import Eighth from './components/eighth/eighth.js';
 
@@ -16,10 +17,12 @@ class App extends Component{
 				<Second />
 				<Third /> 
 				<Fourth />
+				<Sixth />
 				<Eighth />
 				<Tenth />
 			</div>
 		 );
+		
 	}
 }
 
